21b74bc45dc19a200783e08a6cb217d22102780a
Joseph Lee <joseph@Josephs-MacBook-Pro-2.local>
2024-06-11 16:33:49 -0700

    Switch to use vercel function

--- api/main.js
+++ api/main.js
@@ -1,10 +1,8 @@
-require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const port = 80;
 
-app.get("/", async (req, res) => {
+app.get("/api", async (req, res) => {
   try {
     const message = req.query.message;
     const response = await axios.post(process.env.WEBHOOK_URL, { message });
@@ -14,6 +12,4 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+module.exports = app;
