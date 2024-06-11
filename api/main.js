978032f1d3a4dc961690e4c29b5b39d73346819d
Joseph Lee <joseph@Josephs-MacBook-Pro-2.local>
2024-06-11 16:29:30 -0700

    Switch to use port 80

--- api/main.js
+++ api/main.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.get("/", async (req, res) => {
   try {
