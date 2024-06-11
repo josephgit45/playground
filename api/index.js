96c895cd2de48143318eb2729069be6e78d26f37
Joseph Lee <joseph@Josephs-MacBook-Pro-2.local>
2024-06-11 16:49:30 -0700

    Update message returned to be message

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
   try {
     const message = req.query.message;
     const response = await axios.post(process.env.WEBHOOK_URL, { message });
-    res.json({ message: response.data });
+    res.json({ message: message });
   } catch (error) {
     res.status(500).send(error);
   }
